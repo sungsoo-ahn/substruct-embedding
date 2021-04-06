a5034c055f3f615a531ee3a2cafab4da6534cb41
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-04-06 18:44:21 +0400

    script

--- script/node_mask.sh
+++ script/node_mask.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-LOSS_TYPE=$1
-TRANSFORM_TYPE=$2
-RUN_TAG="${LOSS_TYPE}_${TRANSFORM_TYPE}"
+RUN_TAG="nm"
 MODEL_PATH="../resource/result/${RUN_TAG}/model.pt"
 
-echo $LOSS_TYPE
-echo $TRANSFORM_TYPE
+echo $WALK_LENGTH_RATE
 echo $RUN_TAG
 echo $MODEL_PATH
 
-python train_embedding.py --loss_type $LOSS_TYPE --transform_type $TRANSFORM_TYPE --run_tag $RUN_TAG
+python pretrain.py --scheme node_masking --node_mask_rate 0.3 --run_tag $RUN_TAG
 
 for RUNSEED in 0 1 2 3 4 5 6 7 8 9
 do
---
+++ script/subgraph_mask.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WALK_LENGTH_RATE=$1
+RUN_TAG="wlr_${WALK_LENGTH_RATE}"
+MODEL_PATH="../resource/result/${RUN_TAG}/model.pt"
+
+echo $WALK_LENGTH_RATE
+echo $RUN_TAG
+echo $MODEL_PATH
+
+python pretrain.py --scheme subgraph_masking --walk_length_rate $WALK_LENGTH_RATE --run_tag $RUN_TAG
+
+for RUNSEED in 0 1 2 3 4 5 6 7 8 9
+do
+	for DATASET in "tox21" "hiv" "muv" "bace" "bbbp" "toxcast" "sider" "clintox"
+	do
+		python finetune.py --runseed $RUNSEED --dataset $DATASET --model_path $MODEL_PATH
+	done	
+done
