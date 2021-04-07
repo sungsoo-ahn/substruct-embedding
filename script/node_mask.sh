a1f65fa106858b350fce34a37d2d19cd8854d826
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-04-07 15:26:39 +0900

    tmp

--- script/node_mask.sh
+++ script/node_mask.sh
@@ -7,11 +7,11 @@ echo $WALK_LENGTH_RATE
 echo $RUN_TAG
 echo $MODEL_PATH
 
-python pretrain.py --scheme node_masking --node_mask_rate 0.3 --run_tag $RUN_TAG
+#python pretrain.py --scheme node_masking --node_mask_rate 0.3 --run_tag $RUN_TAG
 
 for RUNSEED in 0 1 2 3 4 5 6 7 8 9
 do
-	for DATASET in "tox21" "hiv" "muv" "bace" "bbbp" "toxcast" "sider" "clintox"
+	for DATASET in "tox21" "bace" "bbbp" "toxcast" "sider" "clintox"
 	do
 		python finetune.py --runseed $RUNSEED --dataset $DATASET --model_path $MODEL_PATH
 	done	
