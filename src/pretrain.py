8ffe912953e9e95630a504fd5a635fe163301568
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-04-20 13:10:20 +0400

    tmp

--- src/pretrain.py
+++ src/pretrain.py
@@ -22,7 +22,7 @@ from tqdm import tqdm
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="tox21")
+    parser.add_argument("--dataset", type=str, default="zinc_standard_agent")
     parser.add_argument("--num_epochs", type=int, default=10)
     parser.add_argument("--num_warmup_epochs", type=int, default=1)
     parser.add_argument("--log_freq", type=float, default=10)
