122450ef54d497daa68fca777e417703d21347c4
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-04-30 01:11:18 +0400

    pull

--- script/mask_contrast0.sh
+++ script/mask_contrast0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUN_TAG="edge_contrast0"
+RUN_TAG="mask_contrast_full"
 MODEL_PATH="../resource/result/${RUN_TAG}/model.pt"
 
 echo $RUN_TAG
@@ -8,12 +8,12 @@ echo $MODEL_PATH
 
 python pretrain.py \
 --use_neptune \
---scheme edge_contrast \
---transform edge_mask \
---mask_rate 0.1 \
+--scheme mask_contrast \
+--transform mask \
+--num_epochs 100 \
 --run_tag $RUN_TAG
 
 python finetune.py \
---datasets "bace" "bbbp" "toxcast" "sider" "clintox" "tox21" \
+--datasets "bace" "bbbp" "toxcast" "sider" "clintox" "tox21" "hiv" "muv" \
 --model_path $MODEL_PATH \
 --run_tag $RUN_TAG
