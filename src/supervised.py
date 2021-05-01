4b8aa069dbd8b12658d3e32931d54b6b78274d5b
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-05-01 11:37:05 +0400

    supervised

---
+++ src/supervised.py
@@ -0,0 +1,134 @@
+import argparse
+
+from data.dataset import MoleculeDataset
+from torch_geometric.data import DataLoader
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from tqdm import tqdm
+import numpy as np
+
+#from model import GNN, GNN_graphpred
+from model import GNN, GNN_graphpred
+from sklearn.metrics import roc_auc_score
+
+from data.splitter import scaffold_split, random_split, random_scaffold_split
+
+import neptune.new as neptune
+import os
+
+criterion = nn.BCEWithLogitsLoss(reduction = "none")
+
+def train(args, model, device, loader, optimizer):
+    model.train()
+
+    avg_loss = 0.0
+    
+    for step, batch in enumerate(tqdm(loader)):
+        batch = batch.to(device)
+        pred = model(batch.x, batch.edge_index, batch.edge_attr, batch.batch)
+        y = batch.y.view(pred.shape).to(torch.float64)
+
+        #Whether y is non-null or not.
+        is_valid = y**2 > 0
+        #Loss matrix
+        loss_mat = criterion(pred.double(), (y+1)/2)
+        #loss matrix after removing null target
+        loss_mat = torch.where(is_valid, loss_mat, torch.zeros(loss_mat.shape).to(loss_mat.device).to(loss_mat.dtype))
+            
+        optimizer.zero_grad()
+        loss = torch.sum(loss_mat)/torch.sum(is_valid)
+        loss.backward()
+
+        optimizer.step()
+        
+        avg_loss += (loss.detach().cpu().item()) / len(loader)
+    
+    return avg_loss   
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch implementation of pre-training of graph neural networks')
+    parser.add_argument('--device', type=int, default=0,
+                        help='which gpu to use if any (default: 0)')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='learning rate (default: 0.001)')
+    parser.add_argument('--decay', type=float, default=0,
+                        help='weight decay (default: 0)')
+    parser.add_argument('--num_layer', type=int, default=5,
+                        help='number of GNN message passing layers (default: 5).')
+    parser.add_argument('--emb_dim', type=int, default=300,
+                        help='embedding dimensions (default: 300)')
+    parser.add_argument('--dropout_ratio', type=float, default=0.2,
+                        help='dropout ratio (default: 0.2)')
+    parser.add_argument('--graph_pooling', type=str, default="mean",
+                        help='graph level pooling (sum, mean, max, set2set, attention)')
+    parser.add_argument('--JK', type=str, default="last",
+                        help='how the node features across layers are combined. last, sum, max or concat')
+    parser.add_argument('--dataset', type=str, default = 'chembl_filtered', help='root directory of dataset. For now, only classification.')
+    parser.add_argument('--gnn_type', type=str, default="gin")
+    parser.add_argument('--input_model_path', type=str, default = '', help='filename to read the model (if there is any)')
+    parser.add_argument('--output_model_path', type = str, default = '', help='filename to output the pre-trained model')
+    parser.add_argument('--num_workers', type=int, default = 8, help='number of workers for dataset loading')
+    parser.add_argument('--run_tag', type=str, default='')
+    parser.add_argument('--use_neptune', action="store_true")
+    args = parser.parse_args()
+
+
+    torch.manual_seed(0)
+    np.random.seed(0)
+    device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(0)
+
+    #Bunch of classification tasks
+    if args.dataset == "chembl_filtered":
+        num_tasks = 1310
+    else:
+        raise ValueError("Invalid dataset name.")
+
+    #set up dataset
+    dataset = MoleculeDataset("../resource/dataset/" + args.dataset, dataset=args.dataset)
+
+    loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers = args.num_workers)
+
+    #set up model
+    model = GNN_graphpred(args.num_layer, args.emb_dim, num_tasks, JK = args.JK, drop_ratio = args.dropout_ratio, graph_pooling = args.graph_pooling, gnn_type = args.gnn_type)
+    if not args.input_model_path == "":
+        model.from_pretrained(args.input_model_path)
+    
+    model.to(device)
+
+    #set up optimizer
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.decay)  
+    print(optimizer)
+
+    if args.use_neptune:
+        print("Loading neptune...")
+        run = neptune.init(project="sungsahn0215/ssg", name="supervised")
+        run["parameters"] = vars(args)
+        if args.run_tag == "":
+            run_tag = run["sys/id"].fetch()
+        else:
+            run_tag = args.run_tag
+        os.makedirs(f"../resource/result/{run_tag}", exist_ok=True)
+
+    for epoch in range(1, args.epochs+1):
+        print("====epoch " + str(epoch))
+    
+        loss = train(args, model, device, loader, optimizer)
+        run["train/loss"].log(loss)
+
+    if args.use_neptune:
+        torch.save(model.gnn.state_dict(), f"../resource/result/{run_tag}/supervised.pth")
+
+
+if __name__ == "__main__":
+    main()
