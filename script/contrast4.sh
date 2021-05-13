ecf46fc52d3a0c833296e7be9bfe8c4cdccfb339
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-05-13 17:11:25 +0400

    nomask exp

---
+++ script/contrast4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RUN_TAG="contrast0"
+MODEL_PATH="../resource/result/${RUN_TAG}/model.pt"
+SUPERVISED_MODEL_PATH="../resource/result/${RUN_TAG}/model_supervised.pt"
+
+echo $RUN_TAG
+echo $MODEL_PATH
+echo $SUPERVISED_MODEL_PATH
+
+python pretrain.py \
+--contract_type once \
+--contract_p 0.9 \
+--mask_p 0.0 \
+--use_neptune \
+--run_tag $RUN_TAG
+
+python finetune.py \
+--datasets "bace" "bbbp" "sider" "clintox" "tox21" "toxcast" "hiv" "muv" \
+--model_path $MODEL_PATH \
+--run_tag $RUN_TAG
