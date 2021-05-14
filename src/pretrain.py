2c06b68a37a685a5e2e4ac7e480910f1daa18f62
Sungsoo Ahn <sungsahn0215@gmail.com>
2021-05-14 18:41:27 +0400

    scripts

--- src/pretrain.py
+++ src/pretrain.py
@@ -38,7 +38,7 @@ def train_step(batch, model, optim):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default="zinc_brics")
-    parser.add_argument("--num_epochs", type=int, default=5)
+    parser.add_argument("--num_epochs", type=int, default=20)
     parser.add_argument("--log_freq", type=float, default=100)
 
     parser.add_argument("--scheme", type=str, default="junction_contrastive")
