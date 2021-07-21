7e97438ad02001f592054252a3ff9f44ca23bbac
Sungsoo <sungsahn0215@gmail.com>
2021-07-21 14:20:45 +0400

    tmp

--- src/scheme/predictive.py
+++ src/scheme/predictive.py
@@ -29,12 +29,12 @@ class Model(torch.nn.Module):
         out0 = self.encoder(batch0.x, batch0.edge_index, batch0.edge_attr)
         out0 = out0 + self.action_embedding(batch0.action_feat)
         out0 = self.projector(out0)
-        out0 = global_mean_pool(out0)
+        out0 = global_mean_pool(out0, batch0.batch)
         features0 = torch.nn.functional.normalize(out0, p=2, dim=1)
 
         out1 = self.encoder(batch1.x, batch1.edge_index, batch1.edge_attr)
         out1 = self.projector(out1)
-        out1 = global_mean_pool(out1)
+        out1 = global_mean_pool(out1, batch1.batch)
         features1 = torch.nn.functional.normalize(out1, p=2, dim=1)
 
         logits = torch.matmul(features0, features1.t()) / self.contrastive_temperature
